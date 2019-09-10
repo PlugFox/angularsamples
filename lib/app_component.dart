a63b79438c45fd9aec012abbf6b296f6962a38cf
Plug Fox <plugfox@gmail.com>
2019-09-10 19:38:50 +0300

    add DateRangePicker

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -9,7 +9,10 @@ import 'package:angular_components/angular_components.dart';
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
   directives: [
+    NgIf,
+    NgFor,
     MaterialDateRangePickerComponent,
+    MaterialButtonComponent,
   ],
   providers: [
     datepickerBindings,
@@ -18,23 +21,50 @@ import 'package:angular_components/angular_components.dart';
 class AppComponent {
   AppComponent();
 
-  var one = DatepickerComparison.noComparison(
-    DatepickerDateRange(
-      'first',
+  String _log = '';
+  String get log => this._log;
+  set log(String value) {
+    print(value);
+    this._log += '\n$value';
+  }
+
+  List<DateRangeTile> rangeTileList = <DateRangeTile>[];
+
+  void rmRange() {
+    rangeTileList.removeLast();
+  }
+
+  void addRange() {
+    this.rangeTileList.add(DateRangeTile());
+  }
+
+  void checkDateRange() {
+    log = '';
+    log = '### Ranges:';
+    for (DateRangeTile rangeTile in this.rangeTileList) {
+      log = '${rangeTile.range.toString()}';
+    }
+    log = '### ### ###';
+    log = '';
+  }
+}
+
+class DateRangeTile {
+  DatepickerComparison _range;
+  DatepickerComparison get range => this._range;
+  set range(DatepickerComparison value) {
+    this._range = value;
+  }
+
+  DateRangeTile();
+
+  /*
+  // DatepickerComparison sample
+  DatepickerComparison.previousPeriod(
+    DatepickerDateRange.custom(
       Date.today().add(days: 3),
       Date.today().add(days: 4),
     ),
-  );
-  var two = DatepickerComparison.noComparison(
-    DatepickerDateRange(
-      'second',
-      Date.today().add(days: 5),
-      Date.today().add(days: 6),
-    ),
-  );
-  void checkDateRange() {
-    /// TODO: Получить текущие значения material-date-range-picker
-    print('first: ${one.toString()}');
-    print('second: ${two.toString()}');
-  }
+  ),
+  */
 }
